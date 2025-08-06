93c8988b2fa7679f1270a43446ccb3affa4e5008
charu-chandak-aaft <charu.c@aaft.com>
2025-08-06 12:58:37 +0530

    conflicts-fix

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,15 +1,23 @@
 'use client';
+import Form1 from "../../components/Form1";
+import Cards from "../../components/Cards";
+import Summary from "../../components/Summary";
 import { useSession } from "next-auth/react";
 
 export default function Dashboard() {
-  const { data: session } = useSession();
+   const { data: session } = useSession();
 
   if (!session) return <p>Please login first</p>;
-
   return (
-    <div>
+    <>
+     <div>
       <h1>Welcome, {session.user.name}</h1>
       <p>Email: {session.user.email}</p>
     </div>
+      <h2 className="text-3xl font-bold mb-4 pl-8 text-[#000000] font-[ClashDisplay-Regular]">Dashboard Overview</h2>
+      <Form1 />
+      <Cards />
+      <Summary />
+    </>
   );
 }
